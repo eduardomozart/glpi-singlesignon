d235c9491eb311a52a82e591e376c7291378e219
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-02-03 11:04:22 -0300

    Update extract_template.sh

--- tools/extract_template.sh
+++ tools/extract_template.sh
@@ -8,4 +8,4 @@ copyright='glpi-singlesignon Development Team'
 
 xgettext *.php */*.php --copyright-holder="COPYRIGHT" --package-name="$soft" -o locales/singlesignon.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
 	--keyword=_n:1,2,4t --keyword=__s:1,2t --keyword=__:1,2t --keyword=_e:1,2t --keyword=_x:1c,2,3t \
-	--keyword=_ex:1c,2,3t --keyword=_nx:1c,2,3,5t --keyword=_sx:1c,2,3t
+	--keyword=_ex:1c,2,3t --keyword=_nx:1c,2,3,5t --keyword=_sx:1c,2,3t --keyword=__sso:1,2t
